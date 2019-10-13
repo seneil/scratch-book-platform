b3e21b73e1d55253a3a8eade335b4425f860dfe1
dshster <dmitry@shvalyov.ru>
2019-10-13 17:44:00 +0300

    [SCB-6] Инициализация проекта, настройка окружения.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require('@babel/register')({
+  ignore: [/(node_modules)/],
+  presets: ['@babel/preset-env'],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+  ],
+});
+require('module-alias/register');
