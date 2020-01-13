a8cdb1cc827537a54b0b6c67c20cf5a7d564e47d
dshster <dmitry@shvalyov.ru>
2020-01-13 19:22:31 +0300

    [SCB-13] Разработка резолвера мутации авторизации.

--- src/application/resolvers/mutation.js
+++ src/application/resolvers/mutation.js
@@ -3,6 +3,15 @@ import jwt from 'jsonwebtoken';
 
 const { APPLICATION_SECRET } = process.env;
 
+const responseToken = (response, userId) => {
+  const token = jwt.sign({ userId }, APPLICATION_SECRET);
+
+  response.cookie('token', token, {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+  });
+};
+
 const Mutation = {
   signup: (parent, args, { response, db }, info) => {
     const salt = bcrypt.genSaltSync(10);
@@ -14,14 +23,27 @@ const Mutation = {
         ...args, email, password,
       },
     }, info).then(user => {
-      const token = jwt.sign({ userId: user.id }, APPLICATION_SECRET);
-
-      response.cookie('token', token, {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 365,
-      });
+      responseToken(response, user.id);
 
       return user;
+    });
+  },
+
+  signin: (parent, args, { response, db }) => {
+    const email = args.email.toLowerCase();
+
+    return db.query.user({
+      where: { email },
+    }).then(user => {
+      const isPasswordValid = bcrypt.compareSync(args.password, user.password);
+
+      if (!isPasswordValid) return Promise.reject();
+
+      responseToken(response, user.id);
+
+      return user;
+    }).catch(() => {
+      throw new Error('No such user found');
     });
   },
 };
