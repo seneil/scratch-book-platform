84c1bfbd9ebaeeff5f583c226d5e91fa228f1ecf
dshster <dmitry@shvalyov.ru>
2020-01-19 19:48:08 +0300

    [SCB-16] Добавления обработки запроса notes.

--- src/application/resolvers/query.js
+++ src/application/resolvers/query.js
@@ -1,9 +1,23 @@
+import { forwardTo } from 'prisma-binding';
+
 const Query = {
   me: (parent, args, { request, db }, info) => {
     const { userId } = request;
 
     return userId
       ? db.query.user({ where: { id: userId } }, info)
+      : null;
+  },
+
+  note: forwardTo('db'),
+
+  notes: (parent, args, { request, db }, info) => {
+    const { userId } = request;
+
+    const first = args.first || 5;
+
+    return userId
+      ? db.query.notes({ where: { author: { id: userId } }, ...args, first }, info)
       : null;
   },
 };
