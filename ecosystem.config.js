ef279d0731915a86c5b111ced9b566fa89256a19
dshster <dmitry@shvalyov.ru>
2020-01-21 01:27:42 +0300

    [SCB-17] Конфигурирования раздела deploy.staging pm2.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,28 @@
+require('dotenv').config({ path: 'variables.env' });
+
 module.exports = {
   apps: [{
     name: 'scratch-book-platform',
     script: './src/index.js',
     watch: ['./src'],
     watch_delay: 1000,
+    max_restarts: 5,
+    max_memory_restart: '150M',
     ignore_watch: ['.git', 'node_modules'],
     env: {
       NODE_ENV: 'development',
     },
   }],
+  deploy: {
+    staging: {
+      user: process.env.DEPLOYMENT_USER,
+      host: process.env.DEPLOYMENT_HOST,
+      repo: process.env.DEPLOYMENT_REPO,
+      path: process.env.DEPLOYMENT_PATH,
+      ref: 'origin/develop',
+      'pre-deploy': 'git fetch --all',
+      'post-setup': 'ls -la',
+      'post-deploy': 'npm install --production && pm2 startOrRestart ecosystem.config.js',
+    },
+  },
 };
